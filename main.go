087835871ec00676eab0cae3b6d06f5b81a11704
Ardeshir Sepahsalar <ardeshir.org@gmail.com>
2016-06-01 20:50:05 +0000

    fixed minor typos

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var (
 	port = ":8090"
 )
 
-func startHandler(w http.ResposeWriter, r *http.Request) {
+func startHandler(w http.ResponseWriter, r *http.Request) {
 	err := tmpl.ExecuteTemplate(w, "start.html", nil)
 	if err != nil {
 		log.Fatal("Templates start.html missing")
@@ -22,16 +22,16 @@ func startHandler(w http.ResposeWriter, r *http.Request) {
 
 func middleHandler(w http.ResponseWriter, r *http.Request) {
 	cookieValue := r.PostFormValue("message")
-	cookie := http.Cookie{Name: "message", Value: "message:" + cookieValue, Expires: time.Now().Add(60 * time.Second), HttpOnly: true}
+	cookie := http.Cookie{Name: "message", Value: "message:" + cookieValue, Expires: time.Now().Add(10 * time.Second), HttpOnly: true}
 	http.SetCookie(w, &cookie)
 	http.Redirect(w, r, "/finish", 301)
 }
 
 func finishHandler(w http.ResponseWriter, r *http.Request) {
-	cookieValue, _ := r.Cookie("message")
+	cookieVal, _ := r.Cookie("message")
 
-	if cookieValue != nil {
-		fmt.Fprintln(w, "We got "+string(cookeiValue.Value)+", now try to refesh?!")
+	if cookieVal != nil {
+		fmt.Fprintln(w, "We got "+string(cookieVal.Value)+", now try to refesh?!")
 		cookie := http.Cookie{Name: "message", Value: "", Expires: time.Now(), HttpOnly: true}
 		http.SetCookie(w, &cookie)
 	} else {
