b3337a384891982bc022ab002b781f0f2db87075
Ardeshir Sepahsalar <ardeshir.org@gmail.com>
2016-06-02 23:04:18 +0000

    added good stuff

--- main.go
+++ main.go
@@ -1,23 +1,84 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 )
 
-var (
-	tmpl = template.Must(template.ParseGlob("templates/*"))
-	port = ":8090"
+const (
+	DBHost  = "127.0.0.1"
+	DBPort  = ":3306"
+	DBUser  = "root"
+	DBPass  = "Casey4ever"
+	DBDbase = "CMS"
+	port  = ":8090"
 )
 
-func startHandler(w http.ResponseWriter, r *http.Request) {
-	err := tmpl.ExecuteTemplate(w, "start.html", nil)
+var database *sql.DB
+
+type Page struct {
+	Title      string
+	RawContent string
+	Content    template.HTML
+	Date       string
+	GUID       string
+}
+
+
+func ServeIndex(w http.ResponseWriter, r *http.Request) {
+	var Pages = []Page{}
+	pages, err := database.Query("SELECT page_title, page_content, page_date, page_guid FROM pages ORDER BY ? DESC", "page_date")
 	if err != nil {
-		log.Fatal("Templates start.html missing")
+		fmt.Fprintln(w, err.Error)
 	}
+	defer pages.Close()
+	for pages.Next() {
+		thisPage := Page{}
+		pages.Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date, &thisPage.GUID)
+		thisPage.Content = template.HTML(thisPage.RawContent)
+		Pages = append(Pages, thisPage)
+	}
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, Pages)
+}
+
+func ServePage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageGUID := vars["guid"]
+	thisPage := Page{}
+
+	fmt.Println(pageGUID)
+
+	err := database.QueryRow("SELECT page_title, page_content, page_date FROM pages WHERE page_guid=?",pageGUID).Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+	thisPage.Content = template.HTML(thisPage.RawContent)
+
+	if err != nil {
+		// log.Println("Coudn't get page: " + pageID)
+		// log.Println(err.Error)
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		log.Println("Couldn't get page " + pageGUID)
+		return
+	}
+	/* html := `<html><head><title>` + thisPage.Title + `</title></head><body><h1>` + thisPage.Title + `</h1><div>` +
+		thisPage.Content + `</div></body></html>`
+	fmt.Fprintln(w, html) */
+	t, _ := template.ParseFiles("templates/blog.html")
+	t.Execute(w, thisPage)
+}
+
+func startHandler(w http.ResponseWriter, r *http.Request) {
+	tp, _ := template.ParseFiles("templates/start.html")
+        tp.Execute(w,nil)
+ 
+
+
+
 }
 
 func middleHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,11 +100,28 @@ func finishHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func RedirIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/home", 301)
+}
+
 func main() {
 
-	http.HandleFunc("/start", startHandler)
-	http.HandleFunc("/view", middleHandler)
-	http.HandleFunc("/finish", finishHandler)
+	dbConn := fmt.Sprintf("%s:%s@/%s", DBUser, DBPass, DBDbase)
+	// fmt.Println(dbConn)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("Couldn't connect to " + DBDbase)
+		log.Println(err.Error)
+	}
+	database = db
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/page/{guid:[0-9a-zA\\-]+}", ServePage)
+	routes.HandleFunc("/", RedirIndex)
+	routes.HandleFunc("/home", ServeIndex)
+	routes.HandleFunc("/view", middleHandler)
+	routes.HandleFunc("/finish", finishHandler)
+	http.Handle("/", routes)
 
 	log.Fatal(http.ListenAndServe(port, nil))
 }
